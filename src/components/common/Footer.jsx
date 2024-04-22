5f98f071ac858e4816513d51a27048d13ff89eaa
Jay Prakash Singh <jayprakash199221@gmail.com>
2024-04-22 23:52:14 +0530

    footer info update

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import { BsWhatsapp } from "react-icons/bs";
+import { FiPhone } from "react-icons/fi";
 
 const Footer = ({ theme }) => {
+
+    let phoneNumber = '8563001092';
+    let phoneURL = `tel:${phoneNumber}`;
+    let whatsAppUrl = `https://wa.me/${phoneNumber}`;
+
+    let contactItems = [
+        { text: phoneNumber, url: whatsAppUrl, icon: <BsWhatsapp /> }, { text: phoneNumber, url: phoneURL, icon: <FiPhone /> },
+    ];
+
+    let listItemStyle = { listStyleType: 'none', padding: 0 };
+    
+    let linkStyle = (theme) => ({ color: theme === 'dark' ? 'white' : 'black', textDecoration: "none" });
 
     return (
 
         <div className={`pro-container footer ${theme}`}>
             <Container className='pro-conent'>
-                <h1>Footer</h1>
+                <Row>
+                    <Col lg={4} md={6} sm={12} className='mt-4'>
+                        <h3>Jay Prakash Singh</h3>
+                        <p className='foot-text'>
+                            Hey, I am Jay Prakash Singh. I'm an energetic web designer with a sharp eye for ReactJS, endeavoring to make effective and
+                            outwardly staggering programming arrangements with an IT foundation.
+                        </p>
+                        <ol style={listItemStyle}>
+                            {contactItems.map((item, index) => (
+                                <li key={index}>
+                                    <a href={item.url} target="_blank" rel="noopener noreferrer" style={linkStyle(theme)}>
+                                        {item.icon} &nbsp; +91-{item.text}
+                                    </a>
+                                </li>
+                            ))}
+                        </ol>
+                    </Col>
+                    <Col lg={4} md={6} sm={12} className='mt-4'>
+                        kkjkjkj
+                    </Col>
+                    <Col lg={4} md={6} sm={12} className='mt-4'>
+                        kkjkjkj
+                    </Col>
+                </Row>
             </Container>
         </div>
     )
