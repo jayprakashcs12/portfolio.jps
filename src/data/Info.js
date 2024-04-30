e3aab456098d6c0df3dc1864b91814c63dfb7c87
Jay Prakash Singh <jayprakash199221@gmail.com>
2024-04-30 12:14:36 +0530

    Update Info data

--- src/data/Info.js
+++ src/data/Info.js
@@ -13,7 +13,7 @@ import { BiLogoGmail } from "react-icons/bi";
 
 export let BioData = {
   fullName: "Jay Prakash Singh", roles: ["(Front End Developer)", "(ReactJS Developer)", "(UI Developer)"],
-  github: "https://github.com/jayprakashcs12/", resume: "https://drive.google.com/file/d/19ZKBcu6kLepC_wYcGTMEJculz_3zjFrx/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/19ZKBcu6kLepC_wYcGTMEJculz_3zjFrx/view?usp=sharing",
   description:
     `As a motivated and adaptable front-end developer, I thrive on embracing new challenges. My passion for learning drives me to consistently deliver 
     high-quality results. With a positive attitude and a growth mindset, I am committed to making a meaningful contribution to every project I undertake, 
@@ -31,7 +31,7 @@ export let ContactsItem = [
 ];
 
 export let EducationDetails = [
-  { id: 1, institution: "Dr. A.P.J. Abdul Kalam Technical University (AKTU), Lucknow", degree: "B. Tech", field: "Computer Science", date: "2012-2016" },
+  { id: 1, institution: "Dr. A.P.J. Abdul Kalam Technical University (AKTU), Lucknow", degree: "B.Tech", field: "C.S.E.", date: "2012-2016" },
   { id: 2, institution: "U.P. Board, Prayagraj", degree: "Intermediate", specialization: "PCM", date: "2007-2009" },
   { id: 3, institution: "U.P. Board, Prayagraj", degree: "High School", specialization: "Science", date: "2005-2007" }
 ];
@@ -43,24 +43,20 @@ export let JobSummary = {
     "Have a working knowledge of API integration, and Cross-Browser (Platform Compatibility Issues)",
     "Worked on creating responsive websites for smart devices using responsive design and Media Queries",
     "Extensive experience in developing web pages using HTML, CSS, Bootstrap, JavaScript, and React JS",
-    "Used React-Router to turn an application into a single-page application (SPA)",
-    "Have a working knowledge of AngularJS, ES6, and PHP with CodeIgniter",
-    "Working knowledge of web protocols and standards (HTTP & HTTPS)",
-    "Developed the pages that are compliant with W3C standards"
+    "Used React-Router to turn an application into a single-page application (SPA)", "Have a working knowledge of AngularJS, ES6, and PHP with CodeIgniter",
+    "Working knowledge of web protocols and standards (HTTP & HTTPS)", "Developed the pages that are compliant with W3C standards"
   ]
 }
 
-export let NavRoutes = [{ navTo: '/', navText: 'Home' }, { navTo: '/about-us', navText: 'About Us' }, { navTo: '/skills', navText: 'Skills' },
-{ navTo: '/experience', navText: 'Experience' }, { navTo: '/education', navText: 'Education' },
-{ navTo: '/personal-info', navText: 'Personal Info' }, { navTo: '/contact-us', navText: 'Contact Us' },
+export let NavRoutes = [
+  { navTo: '/', navText: 'Home' }, { navTo: '/about-us', navText: 'About Us' }, { navTo: '/skills', navText: 'Skills' },
+  { navTo: '/experience', navText: 'Experience' }, { navTo: '/education', navText: 'Education' },
+  { navTo: '/personal-info', navText: 'Personal Info' }, { navTo: '/contact-us', navText: 'Contact Us' },
 ];
 
 export let PersonalDetails = [
-  { key: `Father's Name`, value: 'Surendra Singh' },
-  { key: 'Date of Birth', value: '21/08/1992' },
-  { key: 'Gender', value: 'Male' },
-  { key: 'Marital Status', value: 'Single' },
-  { key: 'Nationality', value: 'Indian' },
+  { key: `Father's Name`, value: 'Surendra Singh' }, { key: 'Date of Birth', value: '21/08/1992' }, { key: 'Gender', value: 'Male' }, 
+  { key: 'Marital Status', value: 'Single' }, { key: 'Nationality', value: 'Indian' },
 ];
 
 export let RoutesComponents = [
@@ -80,36 +76,28 @@ export let SocialLinks = [
 ];
 
 export let TechnicalSkills = [
-  { category: 'Web Development', items: ['HTML - 4, 5', 'CSS - 2, 3'], },
-  { category: 'Database', items: ['MongoDB', 'SQL'], },
-  { category: 'Source Code Editor', items: ['Notepad++', 'VS Code'], },
-  { category: 'Framework', items: ['Bootstrap - 3, 4, 5'], },
-  { category: 'Scripting Language', items: ['JavaScript'], },
-  { category: 'Library', items: ['ReactJS'], },
+  { category: 'Web Development', items: ['HTML - 4, 5', 'CSS - 2, 3'], }, { category: 'Database', items: ['MongoDB', 'SQL'], },
+  { category: 'Source Code Editor', items: ['Notepad++', 'VS Code'], },  { category: 'Framework', items: ['Bootstrap - 3, 4, 5'], },
+  { category: 'Scripting Language', items: ['JavaScript'], }, { category: 'Library', items: ['ReactJS'], },
 ];
 
 export let WorkExperience = [
-  { id: 1, company: "Lobotus Technology", 
-    position: "UI Developer", date: "Jan-2021 - Dec-2023",
-    description: [
-      "Involved in micro front-end design and development of the GUI for the EMR platform",
+  { id: 1, company: "Lobotus Technology", position: "UI Developer", date: "Jan-2021 - Dec-2023", description: [
+      "Involved in micro front-end design and development of the GUI for the EMR platform", 
       "Module testing, module integration, and testing all the modules together",
-      "Building reusable components and front-end libraries for future use",
+      "Building reusable components and front-end libraries for future use", 
       "Use mark-up languages like HTML to create user-friendly web pages",
-      "Develop website features that can enhance the user experience",
+      "Develop website features that can enhance the user experience", 
       "Reviewing application requirements and interface designs",
-      "Extensive experience using React-Bootstrap for layout",
-      "Design changes as per client requirements",
-      "Write clean and well-structured code",
-      "Develop responsive web-based UI",
-      "Playing the role of team helper",
+      "Extensive experience using React-Bootstrap for layout", 
+      "Design changes as per client requirements", 
+      "Write clean and well-structured code", 
+      "Develop responsive web-based UI", 
+      "Playing the role of team helper", 
       "Including API integration"
     ]
   },
-  {
-    id: 2, company: "Bangalore Softsell Pvt Ltd",
-    position: "Junior Software Developer", date: "Oct-2019 - Mar-2021",
-    description: [
+  { id: 2, company: "Bangalore Softsell Pvt Ltd", position: "Junior Software Developer", date: "Oct-2019 - Mar-2021", description: [
       "Utilized AngularJS to develop a single-page application, implementing data binding and synchronization with the server, along with HTML, CSS, Bootstrap, AngularJS & PHP",
       "Involved in designing the user experience interface UI/UX strategy, UI requirements, and converting findings into UI designs",
       "Maintaining and optimizing mobile/web applications",
@@ -117,10 +105,7 @@ export let WorkExperience = [
       "Write clean and well-structured codes"
     ]
   },
-  {
-    id: 3, company: "Cybotrix Technology",
-    position: "Web Developer", date: "Sept-2017 - Oct-2019",
-    description: [
+  { id: 3, company: "Cybotrix Technology", position: "Web Developer", date: "Sept-2017 - Oct-2019", description: [
       "Designed HTML templates for use on all pages on the website working with CSS Background, positioning, text, border, margin, padding, and table",
       "Handled cross-browser compatibility issues & features to enhance user experience",
       "Optimize web pages for maximum speed and boost their scalability",
